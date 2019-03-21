cd6e20a0a6ae8f10a910afc4c85a2c21c265133f
christopher <christopher.r.odom@gmail.com>
2019-03-20 22:28:41 -0400

    Added Frak and SalarMultiplication template functions

--- template.cpp
+++ template.cpp
@@ -14,8 +14,21 @@ template <> struct Factorial<0>{
 	static const int result = 1;
 };
 
+template <int N, int D> struct Frak{
+	static const long Num = N;
+	static const long Dem = D;
+};
+
+template <int N, typename F> struct ScalarMultiplication{
+	typedef Frak<N*F::Num, N*F::Dem> result;
+};
+
 int main()
 {
 	std::cout << "Factorial of 7 = " << Factorial<7>::result << std::endl;
+	typedef Frak<2, 3> TwoThirds;
+	typedef ScalarMultiplication<3, TwoThirds>::result ThreeTwoThirds;
+	std::cout << ThreeTwoThirds::Num;
 	return 0;
 }
+
