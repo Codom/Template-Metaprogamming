a703bd0c1a5b8e15b0935a6ead1d6fb8cc7c89d4
christopher <christopher.r.odom@gmail.com>
2019-03-20 20:13:53 -0400

    Added some simple template functions

--- template.cpp
+++ template.cpp
@@ -6,4 +6,16 @@
  */
 #include  <iostream>
 
+template<int N> struct Factorial{
+	static const int result = N*Factorial<N-1>::result;
+};
 
+template <> struct Factorial<0>{
+	static const int result = 1;
+};
+
+int main()
+{
+	std::cout << "Factorial of 7 = " << Factorial<7>::result << std::endl;
+	return 0;
+}
